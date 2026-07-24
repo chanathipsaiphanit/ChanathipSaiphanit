df810e6face6a115715257d4163f1f81d94a8b92
Chanathip Saiphanit <chanathip.sai@ku.th>
2026-07-24 09:54:03 +0700

    วันนี้เรียนสนุกมากครับ

--- assignments/week03/quiz.py
+++ assignments/week03/quiz.py
@@ -1,5 +1,4 @@
 # Complete this program to classify people by age
-age = int(input("Enter age: "))
 
 # Add your if-elif-else statements here
 # 0-12: Child
@@ -8,6 +7,16 @@ age = int(input("Enter age: "))
 # 60+: Senior
 
 # Your code here:
+
+age = int(input("Enter age: "))
+if age <= 12:
+    print("Child")
+elif age <= 19:
+    print("Teenager")
+elif age <= 59:
+    print("Adult")
+else:
+    print("Senior")
 
 
 
---
+++ labs/week03-lab/tset.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult")
+
+    age = float(input("Enter your GPAX: "))
+if age <=1.5:
+    print("You are an retaire")
