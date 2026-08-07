70326ef617352d9b5569b21ae85f1429a432d4b3
Chanathip Saiphanit <chanathip.sai@ku.th>
2026-08-07 10:07:45 +0700

    ส่งการบ้านครับ

--- assignments/week04/2.1.py
+++ assignments/week04/2.1.py
@@ -1,24 +1,12 @@
 
-prices = []
-print("Enter prices of 6 items:")
-for i in range(6):
-    price = int(input(f"Item {i+1}: "))
-    prices.append(price)
-
-budget = int(input("\nEnter total budget: "))
-
-total = 0
-bought = []
-
-for i in range(6):
-    if total + prices[i] <= budget:
-        print(f"Item {i+1} = {prices[i]} --> buy")
-        total += prices[i]
-        bought.append(prices[i])
+scores = []
+for i in range(5):
+    score = int(input(f"กรอกคะแนนนักเรียนคนที่ {i+1}: "))
+    scores.append(score)
+print("\nผลการสอบ")
+for i in range(5):
+    if scores[i] >= 50:
+        print(f"นักเรียนคนที่ {i+1} ได้ {scores[i]} คะแนน : ผ่าน")
     else:
-        print(f"Item {i+1} = {prices[i]} --> cannot buy")
+        print(f"นักเรียนคนที่ {i+1} ได้ {scores[i]} คะแนน : ไม่ผ่าน")
 
-    print(f"Current total = {total}\n")
-print(f"Bought items: {bought}")
-print(f"Total spent: {total}")
-print(f"Remaining budget: {budget - total}")
--- assignments/week04/2.2.py
+++ assignments/week04/2.2.py
@@ -1,12 +1,25 @@
 
-scores = []
-for i in range(5):
-    score = int(input(f"กรอกคะแนนนักเรียนคนที่ {i+1}: "))
-    scores.append(score)
-print("\nผลการสอบ")
-for i in range(5):
-    if scores[i] >= 50:
-        print(f"นักเรียนคนที่ {i+1} ได้ {scores[i]} คะแนน : ผ่าน")
-    else:
-        print(f"นักเรียนคนที่ {i+1} ได้ {scores[i]} คะแนน : ไม่ผ่าน")
+prices = []
+print("Enter prices of 6 items:")
+for i in range(6):
+    price = int(input(f"Item {i+1}: "))
+    prices.append(price)
 
+budget = int(input("\nEnter total budget: "))
+
+total = 0
+bought = []
+
+for i in range(6):
+    if total + prices[i] <= budget:
+        print(f"Item {i+1} = {prices[i]} --> buy")
+        total += prices[i]
+        bought.append(prices[i])
+    else:
+        print(f"Item {i+1} = {prices[i]} --> cannot buy")
+
+    print(f"Current total = {total}\n")
+print(f"Bought items: {bought}")
+print(f"Total spent: {total}")
+print(f"Remaining budget: {budget - total}")
+print(f"Remaining budget: {budget - total}")
